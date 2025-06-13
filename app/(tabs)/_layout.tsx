9f981586708b71cd6c1462323bde9cb31417613c
BiggiBiggi <frelandbenjamin99@gmail.com>
2025-06-13 09:54:11 +0200

    [FEATURES] End of adding functionalities

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,29 +1,31 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
   return (
     <Tabs
-        screenOptions={{
-            tabBarActiveTintColor: '#ffd33d',
-            headerStyle: {
-                backgroundColor: '#25292e',
-              },
-              headerShadowVisible: false,
-              headerTintColor: '#fff',
-              tabBarStyle: {
-              backgroundColor: '#25292e',},
-        }}
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
     >
-      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color, focused}) => (
-        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24}/>
-      ),
-     }} 
-     />
-      <Tabs.Screen name="about" options={{ title: 'About',  tabBarIcon: ({ color, focused}) => (
-        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
-      ),
-     }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Vérification",
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialIcons name="verified" size={24} color="yellow" />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
